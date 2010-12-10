9805e4e083fe235da26fd93df0bea50babaf8ad2
Ken Wakita <wakita@is.titech.ac.jp>
2010-12-10 17:02:56 +0900

    Added an Eclipse project named colorscience.test for the purpose of JUnit testing.

---
+++ test/jp/ac/titech/is/wakitalab/JUnitBasicTest.java
@@ -0,0 +1,11 @@
+package jp.ac.titech.is.wakitalab;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class JUnitBasicTest {
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
