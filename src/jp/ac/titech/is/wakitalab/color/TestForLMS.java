e902864139267236c0e9d77590aef29733c68003
tsuda7 <tsuda7@dhcp-63-214.is.titech.ac.jp>
2010-12-10 15:45:05 +0900

    ブランチのテスト

--- src/jp/ac/titech/is/wakitalab/color/TestForLMS.java
+++ src/jp/ac/titech/is/wakitalab/color/TestForLMS.java
@@ -5,6 +5,8 @@ public class TestForLMS {
     public static void main(String[] args){
         /* Initialize 経由で、Color クラスの nominalWhite を初期化する */
         Initialize.initialize();
+
+        /* LMS(1,2,3) が XYZ の何に変換されるべきかは調査中 */
         LMS c = new LMS(1,2,3);
         System.out.println(c.toString());
         XYZ w = c.XYZ();
