400a8892f899994bc043452ce9e42e1a12023c7f
tsuda7 <tsuda7@dhcp-63-214.is.titech.ac.jp>
2010-12-10 15:42:44 +0900

    LMSクラスの動作確認用クラスを作成

---
+++ src/jp/ac/titech/is/wakitalab/color/TestForLMS.java
@@ -0,0 +1,14 @@
+package jp.ac.titech.is.wakitalab.color;
+
+public class TestForLMS {
+
+    public static void main(String[] args){
+        /* Initialize 経由で、Color クラスの nominalWhite を初期化する */
+        Initialize.initialize();
+        LMS c = new LMS(1,2,3);
+        System.out.println(c.toString());
+        XYZ w = c.XYZ();
+        System.out.println(w.toString());
+    }
+
+}
